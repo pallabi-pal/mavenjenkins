18e1d86d398c56c836a27bd7363b191c57a77992
pallabi-pal <64428933+pallabi-pal@users.noreply.github.com>
2021-06-09 18:11:21 +0530

    Update UITest.java
    updated selenium code

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -11,24 +11,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest 
 {
-
-	@Parameters("Browser")
 	@Test
-	public void startBrowser(String browserName)
+	public void startBrowser( )
 	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			WebDriverManager.chromedriver().setup();
-			 driver=new ChromeDriver();
-		}
-		else if(browserName.contains("Edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			 driver=new EdgeDriver();
-		}
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
